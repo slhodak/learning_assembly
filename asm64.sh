fc4e6079c02d99df681ef050e1bb80790eb8e23f
slhodak <slhodak@gmail.com>
2020-11-03 22:38:09 -0800

    More conversions work; structure folders

--- asm64.sh
+++ asm64.sh
@@ -4,11 +4,13 @@ if [ -z $1 ]; then
   echo "Usage:  ./asm64 <asmMainFile> (no extension)" exit 
   fi
 #  Verify no extensions were entered
-if [ ! -e "$1.asm" ]; then
-  echo "Error, $1.asm not found." 
+if [ ! -e "assembly/$1.asm" ]; then
+  echo "Error, assembly/$1.asm not found." 
   echo "Note, do not enter file extensions."
   exit
-fi 
+fi
 #  Compile, assemble, and link. 
-yasm -Worphan-labels -g dwarf2 -f elf64 $1.asm -l $1.lst 
-ld -g -o $1 $1.o
+yasm -Worphan-labels -g dwarf2 -f elf64 "assembly/$1.asm" -l "list_files/$1.lst" 
+mv "$1.o" "object_files/$1.o"
+ld -g -o $1 "object_files/$1.o"
+mv $1 "bin/$1"
---
+++ assembly/conversions.asm
@@ -0,0 +1,71 @@
+section       .data
+
+; Constants for exiting program
+EXIT_SUCCESS  equ   0
+SYS_EXIT      equ   60
+
+; Vars
+bNumA         db    100
+wNumA         dw    0
+bNumB         db    -100
+bNum3         db    0
+wNumB         dw    0
+wNumC         dw    200
+bNumC         db    0
+qNumD         dq    40000
+wNumD         dw    0
+bNumE         db    9
+qNumE         dq    0
+bNumF         db    13
+qNumF         dq    1000000
+
+; Empty Vars
+dNumA         dd    0
+qNumA         dq    0
+
+section       .text
+global _start
+_start:
+
+  ; Operations
+  mov         ax, -100
+  mov         byte [bNum3], al
+  mov         ax, word [wNumC]
+  mov         byte [bNumC], al
+  ; bNumC = 100
+  mov         rbx, qWord [qNumD]
+  mov         word [wNumD], bx
+  ; wNumD = 40000
+  ; ## Unsigned Widening ##
+  ; Widening memory
+  mov         rdx, 1000000
+  mov         cl, byte [bNumE]
+  mov         rdx, 0
+  mov         dl, cl
+  mov         qWord [qNumE], rdx
+  ; qNumE = 9
+  ; load effective address
+  lea         rsi, qWord [qNumE]
+  ; Widening register alone
+  mov         rcx, 2103000
+  mov         bl, 120
+  mov         rcx, 0
+  mov         cl, bl
+  ; movzx
+  mov         rsi, 2345678
+  movzx       rsi, byte [bNumF]
+  ; ## Signed Widening ##
+  mov         ax, -7
+  cwde  ; signed ax into double-word in eax
+  mov         ax, -26
+  movsx       ebx, ax ; signed src into larger dest (not dw to qw)
+  mov         dWord [dNumA], ebx
+  mov         ecx, -30000
+  movsxd      rcx, ecx ; signed dw into qw specifically
+  mov         qWord [qNumA], rcx
+
+
+last:
+  mov         rax, SYS_EXIT
+  mov         rdi, EXIT_SUCCESS
+  syscall
